04f0b064e8a503dc6eec70e871f830504b42db18
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-28 21:51:04 +0530

    mm

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "java-app"
+  }
+}
+
+resource "aws_db_instance" "rds" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  username             = "postgres"
+  password             = "postgres123"
+  skip_final_snapshot  = true
+}
