cffeedd1d5d3f9c968896efffc39bbd71055891f
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-28 22:53:26 +0530

    m4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 # ---------------- VPC ----------------
@@ -7,43 +7,34 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# ---------------- SUBNET ----------------
-resource "aws_subnet" "subnet" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
 }
 
-# ---------------- INTERNET GATEWAY ----------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-# ---------------- ROUTE TABLE ----------------
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.main.id
+}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "rta" {
-  subnet_id      = aws_subnet.subnet.id
+  subnet_id      = aws_subnet.subnet1.id
   route_table_id = aws_route_table.rt.id
 }
 
 # ---------------- SECURITY GROUP ----------------
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 8080
@@ -59,6 +50,13 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -69,37 +67,37 @@ resource "aws_security_group" "sg" {
 
 # ---------------- EC2 ----------------
 resource "aws_instance" "ec2" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-
-  subnet_id              = aws_subnet.subnet.id
+  subnet_id     = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.sg.id]
 
   associate_public_ip_address = true
 
-  tags = {
-    Name = "java-app"
-  }
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install openjdk-17-jdk -y
+              EOF
 }
 
 # ---------------- RDS ----------------
 resource "aws_db_subnet_group" "db_subnet" {
-  name       = "main-subnet-group"
-  subnet_ids = [aws_subnet.subnet.id]
+  name = "db-subnet"
+  subnet_ids = [aws_subnet.subnet1.id]
 }
 
 resource "aws_db_instance" "rds" {
-  allocated_storage    = 20
-  engine               = "postgres"
-  instance_class       = "db.t3.micro"
-  username             = "postgres"
-  password             = var.db_password
-  skip_final_snapshot  = true
+  allocated_storage = 20
+  engine = "postgres"
+  instance_class = "db.t3.micro"
+  db_name = "mydb"
+  username = "postgres"
+  password = var.db_password
+  skip_final_snapshot = true
 
-  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.sg.id]
-
-  publicly_accessible = true
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
 }
 
 # ---------------- OUTPUTS ----------------
