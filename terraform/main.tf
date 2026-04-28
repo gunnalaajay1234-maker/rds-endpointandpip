71637186017b74adff9ce345f08b168057e27c09
Ajay Goud <gunnalaajay1234@gmail.com>
2026-04-28 22:42:48 +0530

    m4

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,90 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# ---------------- VPC ----------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# ---------------- SUBNET ----------------
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# ---------------- INTERNET GATEWAY ----------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# ---------------- ROUTE TABLE ----------------
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# ---------------- SECURITY GROUP ----------------
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------- EC2 ----------------
 resource "aws_instance" "ec2" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
 
+  subnet_id              = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  associate_public_ip_address = true
+
   tags = {
     Name = "java-app"
   }
+}
+
+# ---------------- RDS ----------------
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "main-subnet-group"
+  subnet_ids = [aws_subnet.subnet.id]
 }
 
 resource "aws_db_instance" "rds" {
@@ -19,5 +96,17 @@ resource "aws_db_instance" "rds" {
   password             = var.db_password
   skip_final_snapshot  = true
 
+  db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
   publicly_accessible = true
+}
+
+# ---------------- OUTPUTS ----------------
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.endpoint
 }
